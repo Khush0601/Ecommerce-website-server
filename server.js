6d5040672abd00c3966a4455326b8decefb2a04f
khushboo kumari <ksingh200601@gmail.com>
2025-01-25 00:24:01 +0530

    connected to server and mongoDB

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
+const AppConfig=require('./configs/app_configs')
+const DbConfig=require('./configs/db_configs')
+const express=require('express')
+const mongoose=require("mongoose")
+const cors=require('cors')
+const bodyParser=require('body-parser')
+const app=express()
+require("dotenv").config()
+//middleware for converting js objects into json
+app.use(bodyParser.json())
+app.use(cors())
 
-console.log('hii')
+//connection to DB:
+ mongoose.connect(DbConfig.Db_URL)
+ const db=mongoose.connection
+ db.on('error',()=>{
+    console.log('error while connecting to db')
+ })
+ db.once('open',()=>{
+    console.log('connected to database')
+ })
+
+//connection to server:
+app.listen(AppConfig.PORT,()=>{
+    console.log('server started on Port:',AppConfig.PORT)
+})
